e8ffed87145a04ec48c1510c4a5ef78258052c61
MadMaxCat <max142001@mail.ru>
2024-02-11 12:48:31 +0300

    first commit

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const __dirname = path.resolve();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/', rootRouter);
 app.set('view engine', 'ejs');
+
+/* ==================================================*/
+/* START */
 
 app.set('port', process.env.PORT || 7777);
 
